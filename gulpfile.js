df7ce7161a221f8b876873d20921b2f9833e6e51
abhisheksliam <abhisheksliam@gmail.com>
2017-03-16 00:33:25 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+// For more information on how to configure a task runner, please visit:
+// https://github.com/gulpjs/gulp
+
+var gulp    = require('gulp');
+var clean   = require('gulp-clean');
+var concat  = require('gulp-concat');
+var rename  = require('gulp-rename');
+var uglify  = require('gulp-uglify');
+var less    = require('gulp-less');
+var csso    = require('gulp-csso');
+var es      = require('event-stream');
+var express = require('express');
+var http    = require('http');
+
+gulp.task('clean', function () {
+    // Clear the destination folder
+    gulp.src('dist/**/*.*', { read: false })
+        .pipe(clean({ force: true }));
+});
+
+gulp.task('copy', function () {
+    // Copy all application files except *.less and .js into the `dist` folder
+    return es.concat(
+        gulp.src(['src/vendor/**/*.*'])
+            .pipe(gulp.dest('dist/vendor')),
+        gulp.src(['src/*.*'])
+            .pipe(gulp.dest('dist'))
+    );
+});
+
+gulp.task('copy-html', function () {
+    // Copy all application files except *.less and .js into the `dist` folder
+    return es.concat(
+        gulp.src(['src/**/*.html'])
+            .pipe(gulp.dest('dist'))
+    );
+});
+
+gulp.task('scripts', function () {
+    return es.concat(
+        // Concatenate, minify and copy all JavaScript (except vendor scripts)
+        gulp.src(['src/js/**/*.js', '!src/vendor/**'])
+            .pipe(concat('app.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist/js'))
+    );
+});
+
+gulp.task('styles', function () {
+    // Compile LESS files
+    return gulp.src('src/less/app.less')
+        .pipe(less())
+        .pipe(rename('app.css'))
+        .pipe(csso())
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('server', function () {
+    // Create a HTTP server for static files
+    var port = 3000;
+    var app = express();
+    var server = http.createServer(app);
+
+    app.use(express.static(__dirname + '/dist'));
+
+    server.on('listening', function () {
+        console.log('Listening on http://locahost:' + server.address().port);
+    });
+
+    server.on('error', function (err) {
+        if (err.code === 'EADDRINUSE') {
+            console.log('Address in use, retrying...');
+            setTimeout(function () {
+                server.listen(port);
+            }, 1000);
+        }
+    });
+
+    server.listen(port);
+});
+
+gulp.task('watch', function () {
+    // Watch .js files and run tasks if they change
+    gulp.watch('src/js/**/*.js', ['scripts']);
+
+    // Watch .less files and run tasks if they change
+    gulp.watch('src/less/**/*.less', ['styles']);
+
+    gulp.watch('src/**/*.html', ['copy-html']);
+
+});
+
+// The dist task (used to store all files that will go to the server)
+gulp.task('dist', ['clean', 'copy', 'scripts', 'styles']);
+
+// The default task (called when you run `gulp`)
+gulp.task('default', ['clean', 'copy', 'scripts', 'styles', 'server', 'watch']);
